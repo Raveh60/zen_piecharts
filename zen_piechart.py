fe08d34cbafc7ac914933c788a5cee1cd6db170d
raveh <raveh60@gmail.com>
2017-06-03 18:34:24 +0200

    add net hash

--- zen_piechart.py
+++ zen_piechart.py
@@ -1,10 +1,11 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import sched, time
+import re
 
 url = "http://zenpools.miningspeed.com/"
 
-update_time = 60
+update_time = 120
 
 out_file = "/var/www/html/index.html"
 
@@ -48,7 +49,21 @@ class Pool:
 	name = ""
 	hashrate = 0.0
 
-def parsePools(tags, outPoolList):
+class NetInfos:
+	poolList = []
+	hashRate = 0.0
+
+#---------------------------------------
+
+def hashtxtToFloat(hashtxt):
+	spaceIdx = hashtxt.index(' ')
+	hash = float(hashtxt[:spaceIdx])
+	if hashtxt[spaceIdx+1:][0] == 'K':
+		hash /= 1000.0
+	return hash
+
+def parsePools(soup, outPoolList):
+	tags = soup.findAll('td')
 	tags_len = len(tags);
 	idx = 0
 	pool = None
@@ -64,12 +79,9 @@ def parsePools(tags, outPoolList):
 			if pool == None:
 				print("ERROR: parsePools ! (1)")
 				return False
+			
 			hashtxt = tags[idx].text
-			spaceIdx = hashtxt.index(' ')
-			hash = float(hashtxt[:spaceIdx])
-			if hashtxt[spaceIdx+1:][0] == 'K':
-				hash /= 1000.0
-			pool.hashrate = hash
+			pool.hashrate = hashtxtToFloat(hashtxt)
 			outPoolList.append(pool)
 			pool = None
 			inc = 6
@@ -79,13 +91,28 @@ def parsePools(tags, outPoolList):
 		idx += inc
 	return True
 
-def getPools(outPoolList):
+#---------------------------------------
+
+def getHashrate(soup):
+	hashtags = soup.findAll(text=re.compile('NET hash'), limit=1)
+	if len(hashtags) > 0:
+		hashtxt = str(hashtags[0].encode('utf-8'))[16:]
+		hashfloat = hashtxtToFloat(hashtxt)
+		print(hashfloat)
+		return hashfloat
+	return 0.0
+
+#---------------------------------------
+
+def getPoolsAndHashrate(outNetInfos):
 	with urllib.request.urlopen(url) as response:
 		soup = BeautifulSoup(response.read(), "html.parser", from_encoding="utf-8")
-		tags = soup.findAll('td')
-		return parsePools(tags, outPoolList)
+		outNetInfos.hashrate = getHashrate(soup)
+		return parsePools(soup, outNetInfos.poolList)
 	print("ERROR: getPools !")
 	return False
+
+#---------------------------------------
 
 def createHtml(poolList):
 	outHtml = html_header;
@@ -94,23 +121,26 @@ def createHtml(poolList):
 		outHtml += html_indent + "['" + pool.name + "',\t" + str(pool.hashrate) + "],\n"
 	outHtml += html_footer;
 	return outHtml
+
+#---------------------------------------
 	
 def writeHtml(htmlText):
 	with open(out_file, "w") as text_file:
 		print(htmlText, file=text_file)
-	
-	
+
+#---------------------------------------
 
 # MAIN
 
 s = sched.scheduler(time.time, time.sleep)
 def updatePools(sc): 
-	poolList = []
-	if getPools(poolList) and len(poolList) > 0:
-		print("Found", len(poolList), "pools!")
-		htmlText = createHtml(poolList)
+	netInfos = NetInfos()
+	if getPoolsAndHashrate(netInfos) and len(netInfos.poolList) > 0:
+		print("Found", len(netInfos.poolList), "pools!")
+		htmlText = createHtml(netInfos.poolList)
+		htmlText = htmlText.replace("TITLE", "Zencash network hasrate : " + str(netInfos.hashrate) + " MSol/s")
 		writeHtml(htmlText)
 	s.enter(update_time, 1, updatePools, (sc,))
 
-s.enter(update_time, 1, updatePools, (s,))
+s.enter(1, 1, updatePools, (s,))
 s.run()
